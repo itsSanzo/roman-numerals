cf3d85e02a23c8b2c0fae596acc1b24e54104b64
itsmurasaki <itsmurasaki@gmail.com>
2014-05-19 15:51:49 +0100

    heavy refactor

--- lib/romannumerals.rb
+++ lib/romannumerals.rb
@@ -1,31 +1,19 @@
-
+ROMAN_NUMERALS = { 
+10 => "X",
+9 => "IX",
+5 => "V",
+4 => "IV",
+1 => "I"
+}
 
 def convert(number)
   result = ""
   
-  while number >= 10
-    result << "X"
-    number -= 10
-  end
-
-  while number >= 9
-    result << "IX"
-    number -= 9
-  end
-
-  while number >= 5
-    result << "V"
-    number -= 5
-  end
-
-  while number >= 4
-    result << "IV"
-    number -= 4
-  end
-
-  while number >= 1
-    result << "I"
-    number -= 1
+  ROMAN_NUMERALS.each do |arab, roman|
+    while number >= arab
+      result << roman
+      number -= arab
+    end
   end
 
   result
